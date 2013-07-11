fffafe41ffe098c901970e75a3ee45d7d0df2ec7
unknown <bmcgarrsa@WKS-87928.corp.agp.ads>
2013-07-11 10:56:03 -0400

    Working on Layout

--- brendans-template.tpl.php
+++ brendans-template.tpl.php
@@ -5,15 +5,30 @@
     <div id="weatherBlockMiddle">
         <div id='weatherBlockLeft'>
             <span id='currentWeatherText'>Current</span>
+            </br>
             <img src='http://img.weather.weatherbug.com/forecast/icons/localized/50x42/en/trans/<?php print $content["currentCondCode"]; ?>.png'/>
             </br>
-            <span id='currentWeatherTempText'><?php print $content["currentTemp"]; ?></span>
+            <span id='currentWeatherTempText'><?php print $content["currentTemp"]; ?>&deg;F</span>
+            </br>
+            <span id='currentWeatherDescText'><?php print $content["currentWeatherDesc"]; ?></span>
         </div>
         <div id='weatherBlockRight'>
-            <span id='todayHighText'>Today High</span>
-            <span id='todayHighTempText'><?php print $content["todayHigh"]; ?></span>
-            <span id='todayHighText'>Today Low</span>
-            <span id='todayHighTempText'><?php print $content["todayLow"]; ?></span>
+            <div id="todayForecast">
+                <span id='todayText'>Today</span>
+                </br>
+                <img src='http://img.weather.weatherbug.com/forecast/icons/localized/25x21/en/trans/<?php print $content["todayIcon"]; ?>.png'/>
+                <span id='todayHighAndLow'><?php print $content["todayHigh"]; ?>...<?php print $content["todayLow"]; ?></span>
+                <br>
+                <span id='todayWeatherDescText'><?php print $content["todayDesc"]; ?></span>
+            </div>
+            <div id="tomorrowForecast">
+                <span id='tomorrowText'>Tomorrow</span>
+                </br>
+                <img src='http://img.weather.weatherbug.com/forecast/icons/localized/25x21/en/trans/<?php print $content["tomorrowIcon"]; ?>.png'/>
+                <span id='tomorrowHighAndLow'><?php print $content["tomorrowHigh"]; ?>...<?php print $content["tomorrowLow"]; ?></span>
+                </br>
+                <span id='tomorrowWeatherDescText'><?php print $content["tomorrowDesc"]; ?></span>
+            </div>
         </div>
     </div>
 </div>
